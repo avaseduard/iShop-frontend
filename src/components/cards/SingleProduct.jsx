91b405f2b634d5b0a9050d8966c948c061206f39
eduard <avas.eduard@gmail.com>
2023-12-15 20:58:48 +0200

    improved mobile responsiveness for home page, single product page, home swiper carousel and single product carousel

--- src/components/cards/SingleProduct.jsx
+++ src/components/cards/SingleProduct.jsx
@@ -1,20 +1,19 @@
-import { Card, Tabs, Tooltip } from 'antd'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import _ from 'lodash'
 import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons'
-import { Link } from 'react-router-dom'
 import { Carousel } from 'react-responsive-carousel'
-import pictureunavailable from '../../../src/images/picture_unavailable.jpg'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import ProductListItems from './ProductListItems'
 import StarRatings from 'react-star-ratings'
 import RatingModal from '../modal/RatingModal'
-import { useDispatch, useSelector } from 'react-redux'
-import { useState } from 'react'
+import showAverageRating from '../rating/ShowAverageRating'
 import { setCart } from '../../store/reducers/cart.reducer'
-import _ from 'lodash'
 import { setDrawerVisibility } from '../../store/reducers/drawer.reducer'
 import { addToWishlist } from '../../functions/user'
 import { toast } from 'react-toastify'
-import showAverageRating from '../rating/ShowAverageRating'
+import { Card, Tabs, Tooltip } from 'antd'
+import pictureunavailable from '../../../src/images/picture_unavailable.jpg'
 
 const { Meta } = Card
 
@@ -52,92 +51,98 @@ const SingleProduct = ({ product, onStarClick, star }) => {
   }
 
   return (
-    <>
-      <div className='col-md-7'>
-        {images?.length ? (
-          <Carousel
-            width='560px'
-            showArrows={false}
-            showStatus={false}
-            autoPlay
-            infiniteLoop={true}
-          >
-            {images.map(image => (
-              <img src={image.url} key={image.public_id} />
-            ))}
-          </Carousel>
-        ) : (
-          <Card
-            cover={
-              <img
-                src={pictureunavailable}
-                style={{ height: '560px', objectFit: 'cover' }}
-                className='p-1'
-              />
-            }
-          />
-        )}
-
-        <Tabs
-          defaultActiveKey='1'
-          type='card'
-          size='small'
-          items={[
-            {
-              label: 'Description',
-              key: 1,
-              children: `${description}`,
-            },
-            {
-              label: 'More',
-              key: 2,
-              children:
-                'If you would like to learn more, give us a call on 0123 456 789!',
-            },
-          ]}
-        />
-      </div>
-
-      <div className='col-md-5'>
-        <h1 className='bg-info p-3'>{title}</h1>
-
-        {product.ratings?.length > 0 ? (
-          showAverageRating(product)
-        ) : (
-          <div className='text-center pt-2 pb-3'>No rating yet</div>
-        )}
-
-        <Card
-          actions={[
-            <>
-              <Tooltip title={tooltip}>
-                <a onClick={handleAddToCart}>
-                  <ShoppingCartOutlined className='text-danger' />
-                  <br />
-                </a>
-                Add to cart
-              </Tooltip>
-              <a onClick={handleAddToWishlist}>
-                <HeartOutlined className='text-info' /> <br /> Add to wishlist
-              </a>
-              <RatingModal>
-                <StarRatings
-                  name={_id}
-                  numberOfStars={5}
-                  rating={star}
-                  starRatedColor='blue'
-                  isSelectable={true}
-                  starDimension='24px'
-                  changeRating={onStarClick}
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-7'>
+          {images?.length ? (
+            <Carousel
+              width='100%' // Adjusted to 100% width for responsiveness
+              showArrows={false}
+              showStatus={false}
+              autoPlay
+              infiniteLoop={true}
+            >
+              {images.map(image => (
+                <img
+                  className='img-fluid'
+                  src={image.url}
+                  key={image.public_id}
                 />
-              </RatingModal>
-            </>,
-          ]}
-        >
-          <ProductListItems product={product} />
-        </Card>
+              ))}
+            </Carousel>
+          ) : (
+            <Card
+              cover={
+                <img
+                  src={pictureunavailable}
+                  style={{ height: 'auto', objectFit: 'cover', width: '100%' }}
+                  className='p-1'
+                />
+              }
+            />
+          )}
+
+          <Tabs
+            defaultActiveKey='1'
+            type='card'
+            size='small'
+            items={[
+              {
+                label: 'Description',
+                key: 1,
+                children: `${description}`,
+              },
+              {
+                label: 'More',
+                key: 2,
+                children:
+                  'If you would like to learn more, give us a call on 0123 456 789!',
+              },
+            ]}
+          />
+        </div>
+
+        <div className='col-md-5'>
+          <h1 className='bg-info p-3'>{title}</h1>
+
+          {product.ratings?.length > 0 ? (
+            showAverageRating(product)
+          ) : (
+            <div className='text-center pt-2 pb-3'>No rating yet</div>
+          )}
+
+          <Card
+            actions={[
+              <>
+                <Tooltip title={tooltip}>
+                  <a onClick={handleAddToCart}>
+                    <ShoppingCartOutlined className='text-danger' />
+                    <br />
+                  </a>
+                  Add to cart
+                </Tooltip>
+                <a onClick={handleAddToWishlist}>
+                  <HeartOutlined className='text-info' /> <br /> Add to wishlist
+                </a>
+                <RatingModal>
+                  <StarRatings
+                    name={_id}
+                    numberOfStars={5}
+                    rating={star}
+                    starRatedColor='blue'
+                    isSelectable={true}
+                    starDimension='24px'
+                    changeRating={onStarClick}
+                  />
+                </RatingModal>
+              </>,
+            ]}
+          >
+            <ProductListItems product={product} />
+          </Card>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/components/home/HomeCarousel.jsx
+++ src/components/home/HomeCarousel.jsx
@@ -1,3 +1,55 @@
+// import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules'
+// import { Swiper, SwiperSlide } from 'swiper/react'
+// import { useState, useEffect } from 'react'
+// import { useSelector } from 'react-redux'
+// import { getAllImagesUrls } from '../../functions/carousel'
+
+// import 'swiper/css'
+// import 'swiper/css/navigation'
+// import 'swiper/css/pagination'
+// import 'swiper/css/autoplay'
+
+// const HomeCarousel = () => {
+//   const { user } = useSelector(state => ({ ...state }))
+//   const [images, setImages] = useState([])
+
+//   useEffect(() => {
+//     loadImages()
+//   }, [])
+
+//   // Load all carousel images from db
+//   const loadImages = () =>
+//     getAllImagesUrls()
+//       .then(res => {
+//         setImages(res.data.map(image => image.images))
+//       })
+//       .catch(error => console.log('GET ALL IMAGES FAILED FE -->', error))
+
+//   return (
+//     <Swiper
+//       modules={[Navigation, Pagination, A11y, Autoplay]}
+//       spaceBetween={50}
+//       slidesPerView={1}
+//       autoplay={{
+//         delay: 2000,
+//         disableOnInteraction: false,
+//       }}
+//       loop={true}
+//       navigation
+//       pagination={{ clickable: true }}
+//       className='text-center mt-5'
+//     >
+//       {images.map(image => (
+//         <SwiperSlide key={image.public_id}>
+//           <img src={image.url} alt='' />
+//         </SwiperSlide>
+//       ))}
+//     </Swiper>
+//   )
+// }
+
+// export default HomeCarousel
+
 import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { useState, useEffect } from 'react'
@@ -26,25 +78,31 @@ const HomeCarousel = () => {
       .catch(error => console.log('GET ALL IMAGES FAILED FE -->', error))
 
   return (
-    <Swiper
-      modules={[Navigation, Pagination, A11y, Autoplay]}
-      spaceBetween={50}
-      slidesPerView={1}
-      autoplay={{
-        delay: 2000,
-        disableOnInteraction: false,
-      }}
-      loop={true}
-      navigation
-      pagination={{ clickable: true }}
-      className='text-center mt-5'
-    >
-      {images.map(image => (
-        <SwiperSlide key={image.public_id}>
-          <img src={image.url} alt='' />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className='container'>
+      <Swiper
+        modules={[Navigation, Pagination, A11y, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        navigation
+        pagination={{ clickable: true }}
+        className='text-center mt-5'
+      >
+        {images.map(image => (
+          <SwiperSlide key={image.public_id}>
+            <div className='row'>
+              <div className='col'>
+                <img className='img-fluid' src={image.url} />
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   )
 }
 
